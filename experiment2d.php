e2323a92e2029dab643b52dcbbbf14fb882589f6
dwaard <daan.de.waard@hz.nl>
2018-10-12 10:34:37 +0200

    added experiment 5 fixed wrong titles

--- experiment2d.php
+++ experiment2d.php
@@ -3,15 +3,16 @@
 	<head><title>Experiment 2d</title></head>
 	<body>
 		<h1>HTML Forms explained</h1>
-		<form method="GET">
+		<form method="POST">
         Your name: <input type="text" name="name"><br>
+        Your password: <input type="password" name="pwd"><br>
         <input type="submit" value="Send"></input>
 		</form>
 		<p>
             <?php
-                if ( isset( $_GET['name'] ) ) 
+                if ( isset( $_POST['name'] ) ) 
                 {
-                    echo "Hello ".$_GET['name'];
+                    echo "Hello ".$_POST['name'];
                 } 
             ?>
         </p>
